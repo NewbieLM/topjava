8ce907c4c6955a3c3168fcda3a5bf97100fc1e8e
Maksym Lenets <maximlenets@gmail.com>
2018-05-13 13:15:15 +0300

    HW06_p.1

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -7,4 +7,26 @@ import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+  /*  @Autowired
+    private Environment env;
+
+    private static Environment environment;
+
+    @PostConstruct
+    public void init(){
+        environment = env;
+    }
+
+    @BeforeClass
+    public static void initJpaUtil() {
+        Assume.assumeTrue(!isActiveProfilJdbc());
+    }
+
+    public static boolean isActiveProfilJdbc() {
+        for (String s : environment.getActiveProfiles()) {
+            if ("JDBC".equalsIgnoreCase(s))
+                return true;
+        }
+        return false;
+    }*/
 }
